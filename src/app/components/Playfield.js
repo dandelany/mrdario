8a950944447a62d0e65eaeecf7d2ed22088d5058
Dan Delany <dand@spotify.com>
2016-05-15 19:06:47 -0400

    cleanup Playfield

--- src/app/components/Playfield.js
+++ src/app/components/Playfield.js
@@ -1,14 +1,10 @@
 import _ from 'lodash';
 import React from 'react';
-import cx from 'classnames';
 import { GRID_OBJECTS } from 'constants';
 
 import virusOrange from 'raw!app/svg/virus_orange.svg';
 import virusPurple from 'raw!app/svg/virus_purple.svg';
 import virusGreen from 'raw!app/svg/virus_green.svg';
-//import pillOrange from 'raw!app/svg/pill_orange.svg';
-//import pillPurple from 'raw!app/svg/pill_purple.svg';
-//import pillGreen from 'raw!app/svg/pill_green.svg';
 import destroyed from 'raw!app/svg/destroyed.svg';
 
 import pillHalfOrange from 'raw!app/svg/pill_half_orange.svg';
@@ -33,52 +29,6 @@ const pillHalfRotations = {
   [GRID_OBJECTS.PILL_LEFT]: 270
 };
 
-const Playfield = React.createClass({
-  getDefaultProps() {
-    return {
-      cellSize: 36
-    }
-  },
-  render() {
-    const numRows = this.props.grid.length;
-    const numCols = this.props.grid[0].length;
-    const cellSize = this.props.cellSize;
-
-    return <div
-      className="game-playfield"
-      style={{position: 'relative', width: numCols * cellSize, height: numRows * cellSize}}
-    >
-
-      <svg style={{width: numCols * cellSize, height: numRows * cellSize}}>
-        {this.props.grid.map((row, rowI) => {
-          return row.map((cell, colI) => {
-            let svgString;
-            let transform = '';
-            transform += `translate(${colI * cellSize}, ${rowI * cellSize})`;
-
-            if(cell.type === GRID_OBJECTS.EMPTY) return;
-            else if(cell.type === GRID_OBJECTS.VIRUS) {
-              svgString = viruses[cell.color % viruses.length];
-            } else if(cell.type === GRID_OBJECTS.DESTROYED) {
-              svgString = destroyed;
-            } else if(cell.type === GRID_OBJECTS.PILL_SEGMENT) {
-              svgString = pillSegments[cell.color % pillSegments.length];
-            } else if(_.includes(pillHalfTypes, cell.type)) {
-              svgString = pillHalves[cell.color % pillSegments.length];
-              transform += `rotate(${pillHalfRotations[cell.type]} ${cellSize/2} ${cellSize/2})`;
-            }
-
-            return svgString ? makeSvg(svgString,
-              {transform},
-              {width: cellSize, height: cellSize}
-            ) : null;
-          })
-        })}
-      </svg>
-    </div>;
-  }
-});
-
 function createMarkup(html) { return {__html: html}; }
 
 function makeSvg(svgString, gProps, svgProps) {
@@ -87,4 +37,48 @@ function makeSvg(svgString, gProps, svgProps) {
   </g>;
 }
 
-export default Playfield;
+export default class Playfield extends React.Component {
+  static defaultProps = {
+    cellSize: 36
+  };
+
+  render() {
+    const numRows = this.props.grid.length;
+    const numCols = this.props.grid[0].length;
+    const cellSize = this.props.cellSize;
+    const style = {position: 'relative', width: numCols * cellSize, height: numRows * cellSize};
+
+    return <div {...{style, className: 'game-playfield'}}>
+      <svg style={{width: numCols * cellSize, height: numRows * cellSize}}>
+
+        {this.props.grid.map((row, rowI) => {
+          return _.compact(row.map((cell, colI) => {
+            // make individual SVGs for each non-empty grid element
+            if(cell.type === GRID_OBJECTS.EMPTY) return null;
+
+            let svgString;
+            let transform = `translate(${colI * cellSize}, ${rowI * cellSize})`;
+
+            if(cell.type === GRID_OBJECTS.VIRUS) {
+              svgString = viruses[cell.color % viruses.length];
+
+            } else if(cell.type === GRID_OBJECTS.DESTROYED) {
+              svgString = destroyed;
+
+            } else if(cell.type === GRID_OBJECTS.PILL_SEGMENT) {
+              svgString = pillSegments[cell.color % pillSegments.length];
+
+            } else if(_.includes(pillHalfTypes, cell.type)) {
+              svgString = pillHalves[cell.color % pillSegments.length];
+              transform += `rotate(${pillHalfRotations[cell.type]} ${cellSize/2} ${cellSize/2})`;
+            }
+
+            return svgString ?
+              makeSvg(svgString, {transform}, {width: cellSize, height: cellSize})
+              : null;
+          }))
+        })}
+      </svg>
+    </div>;
+  }
+}
