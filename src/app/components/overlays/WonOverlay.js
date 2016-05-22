8ea8455b94e921da2b8ad6ce5396e2a9a6dedc5d
Dan Delany <dand@spotify.com>
2016-05-21 23:19:24 -0400

    modularize win/lose overlays

---
+++ src/app/components/overlays/WonOverlay.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+import MayaNumeral from 'app/components/MayaNumeral';
+
+const WonOverlay = (props) => {
+  const {style, params} = props;
+  const level = parseInt(params.level || 0);
+  const speed = parseInt(params.speed || 0);
+  const nextLevelPath = `/game/level/${level + 1}/speed/${speed}`;
+
+  return <div className="game-overlay" style={style}>
+    <div className="win-lose-symbol win-symbol">
+      <MayaNumeral value={level} size={40}/>
+      <h2>WIN</h2>
+    </div>
+
+    <div>
+      <Link to={nextLevelPath}>
+        <span className="btn-white">
+          <div className="btn-maya-numeral" style={{marginBottom: 10}}>
+            <MayaNumeral value={level + 1} size={20}/>
+          </div>
+          Next Level &raquo;
+        </span>
+      </Link>
+    </div>
+    <div>
+      <Link to="/">
+        <span className="btn-white">Back to Menu</span>
+      </Link>
+    </div>
+  </div>;
+};
+
+export default WonOverlay;
--- src/app/components/pages/SinglePlayerGame.js
+++ src/app/components/pages/SinglePlayerGame.js
@@ -13,65 +13,8 @@ import Playfield from 'app/components/Playfield';
 import MayaNumeral from 'app/components/MayaNumeral';
 import responsiveGame from 'app/components/responsiveGame';
 
-const WonOverlay = (props) => {
-  const {style, params} = props;
-  const level = parseInt(params.level || 0);
-  const speed = parseInt(params.speed || 0);
-  const nextLevelPath = `/game/level/${level + 1}/speed/${speed}`;
-
-  return <div className="game-overlay" style={style}>
-    <div className="win-lose-symbol win-symbol">
-      <MayaNumeral value={level} size={40}/>
-      <h2>WIN</h2>
-    </div>
-
-    <div>
-      <Link to={nextLevelPath}>
-        <span className="btn-white">
-          <div className="btn-maya-numeral" style={{marginBottom: 10}}>
-            <MayaNumeral value={level + 1} size={20}/>
-          </div>
-          Next Level &raquo;
-        </span>
-      </Link>
-    </div>
-    <div>
-      <Link to="/">
-        <span className="btn-white">Back to Menu</span>
-      </Link>
-    </div>
-  </div>;
-};
-
-const LostOverlay = (props) => {
-  const {style, params} = props;
-  const level = parseInt(params.level || 0);
-  const speed = parseInt(params.speed || 0);
-  const thisLevelPath = `/game/level/${level}/speed/${speed}`;
-
-  return <div className="game-overlay" style={style}>
-    <div className="win-lose-symbol lose-symbol">
-      <MayaNumeral value={params.level} size={40}/>
-      <h2>GAME OVER</h2>
-    </div>
-
-    <div>
-      <Link to={thisLevelPath}>
-        <span className="btn-white">
-          <div className="btn-maya-numeral" style={{marginBottom: 10}}>
-            <MayaNumeral value={level} size={20}/>
-          </div>
-          Try Again
-        </span>
-      </Link>
-    </div>
-    <div>
-      <Link to="/">
-        <span className="btn-white">Back to Menu</span>
-      </Link>
-    </div>
-  </div>
-};
+import WonOverlay from 'app/components/overlays/WonOverlay';
+import LostOverlay from 'app/components/overlays/LostOverlay';
 
 class SinglePlayerGame extends React.Component {
   static defaultProps = {
@@ -96,17 +39,11 @@ class SinglePlayerGame extends React.Component {
   componentWillReceiveProps(newProps) {
     const {params} = this.props;
     const hasChanged = (key) => _.get(this.props, key) !== _.get(newProps, key);
-
-    const shouldInitGame = hasChanged('params.level') || hasChanged('params.speed') ||
+    const shouldInitGame =
+      hasChanged('params.level') || hasChanged('params.speed') ||
       (hasChanged('params.mode') && !newProps.params.mode);
 
     if(shouldInitGame) this._initGame(newProps);
-
-    // if(hasChanged('params.level') || hasChanged('params.level')) this._initGame();
-    // else if(hasChanged('params.mode') && !newProps.params.mode)
-    // console.log('level', hasChanged('params.level'));
-    // console.log('speed', hasChanged('params.speed'));
-    // console.log('mode', hasChanged('params.mode'));
   }
 
   shouldComponentUpdate(newProps, newState) {
@@ -115,11 +52,6 @@ class SinglePlayerGame extends React.Component {
       !shallowEqual(newProps, this.props);
 
     return hasChanged;
-    // console.log(newState);
-    // if(shallowEqual(this.props, newProps) && shallowEqual(this.state, newState)) console.log('YO');
-    // console.log('props', shallowEqual(this.props, newProps), 'state', shallowEqual(this.state, newState));
-    // console.log(deepDiff(this.state, newState));
-    return true;
   }
 
 
@@ -155,7 +87,6 @@ class SinglePlayerGame extends React.Component {
     const {cellSize, params} = this.props;
     // pass fractional padding to set padding to a fraction of cell size
     const padding = (padding < 1) ? this.props.padding * cellSize : this.props.padding;
-    // const numRows = _.get(gameState, 'grid.length', 16);
     const numRows = gameState ? gameState.grid.size : 16;
     const numCols = gameState ? gameState.grid.get(0).size : 8;
     const width = (numCols * cellSize) + (padding * 2);
@@ -163,7 +94,6 @@ class SinglePlayerGame extends React.Component {
 
     const style = {position: 'relative', width, height, padding};
     const overlayStyle = {position: 'absolute', width, height, padding, left: 0};
-
 
     const lostOverlayStyle = {...overlayStyle, top: (params.mode === "lost") ? 0 : height};
     const wonOverlayStyle = {...overlayStyle, top: (params.mode === "won") ? 0 : height};
