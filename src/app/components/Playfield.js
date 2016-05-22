afd2e45e7cd47c8d9c6bb2a9b27955b8e94b0179
Dan Delany <dand@spotify.com>
2016-05-22 03:19:00 -0400

    cleanup

--- src/app/components/Playfield.js
+++ src/app/components/Playfield.js
@@ -7,23 +7,22 @@ import { GRID_OBJECTS } from 'constants';
 import virusOrange from 'raw!app/svg/virus_orange.svg';
 import virusPurple from 'raw!app/svg/virus_purple.svg';
 import virusGreen from 'raw!app/svg/virus_green.svg';
-import destroyed from 'raw!app/svg/destroyed.svg';
-
 import pillHalfOrange from 'raw!app/svg/pill_half_orange.svg';
 import pillHalfPurple from 'raw!app/svg/pill_half_purple.svg';
 import pillHalfGreen from 'raw!app/svg/pill_half_green.svg';
 import pillSegmentOrange from 'raw!app/svg/pill_segment_orange.svg';
 import pillSegmentPurple from 'raw!app/svg/pill_segment_purple.svg';
 import pillSegmentGreen from 'raw!app/svg/pill_segment_green.svg';
+import destroyed from 'raw!app/svg/destroyed.svg';
 
 
 const viruses = [virusOrange, virusPurple, virusGreen];
 const pillHalves = [pillHalfOrange, pillHalfPurple, pillHalfGreen];
 const pillSegments = [pillSegmentOrange, pillSegmentPurple, pillSegmentGreen];
 
-const pillHalfTypes = [
-  GRID_OBJECTS.PILL_TOP, GRID_OBJECTS.PILL_BOTTOM, GRID_OBJECTS.PILL_LEFT, GRID_OBJECTS.PILL_RIGHT
-];
+const pillHalfTypes =
+  [GRID_OBJECTS.PILL_TOP, GRID_OBJECTS.PILL_BOTTOM, GRID_OBJECTS.PILL_LEFT, GRID_OBJECTS.PILL_RIGHT];
+
 const pillHalfRotations = {
   [GRID_OBJECTS.PILL_TOP]: 0,
   [GRID_OBJECTS.PILL_RIGHT]: 90,
@@ -56,10 +55,7 @@ export default class Playfield extends React.Component {
     const width = numCols * cellSize;
     const height = numRows * cellSize;
 
-    const style = {position: 'relative', width, height};
-    const overlayStyle = {position: 'absolute', width, height, top: -height, background: 'red'};
-
-    return <svg style={{width: numCols * cellSize, height: numRows * cellSize}}>
+    return <svg style={{width, height}}>
       {grid.map((row, rowI) => {
         return _.compact(row.map((cell, colI) => {
           // make individual SVGs for each non-empty grid element
